381872b40b26c37a6dcfa31b9832d42ec67e9f25
Vincent Hunter <jhearst29@gmail.com>
2026-05-03 22:20:35 -0400

    Added a Navigation Link to allow the user to press the dinosaurs and move to a new screen; added a search bar and search functionality so the dinosaurs can appear/disapper as the name is typed in searh bar

--- Jurassopedia/ContentView.swift
+++ Jurassopedia/ContentView.swift
@@ -10,33 +10,51 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator] {
+        return predators.searc(for: searchText)
+    }
+    
     var body: some View {
-        List(predators.apexPreadators) { predator in
-            HStack {
-                //Dinosaur Image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:100)
-                    .shadow(color:.white, radius: 1.0)
-                VStack(alignment: .leading){
-                    //Dinosaur Name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    
-                    //Dinosaur Type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
-                        
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        //Dinosaur Image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100, height:100)
+                            .shadow(color:.white, radius: 1.0)
+                        VStack(alignment: .leading){
+                            //Dinosaur Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            
+                            //Dinosaur Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                            
+                        }
+                    }
                 }
+                
             }
-            
+            .navigationTitle(Text("Jurassopedia"))
+            .searchable(text: $searchText,placement: .navigationBarDrawer)
+            .autocorrectionDisabled(true)
+            .animation(.default, value:searchText)
         }
         .preferredColorScheme(.dark)
     }
