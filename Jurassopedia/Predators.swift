ee99116bd34d6e01ba6f7e835b6a8ab40c1c3fff
Vincent Hunter <jhearst29@gmail.com>
2026-05-03 22:22:08 -0400

    Added functions for searching the apex predators list by filter and sorting the apex predators by movie appearanc /id in JSON

--- Jurassopedia/Predators.swift
+++ Jurassopedia/Predators.swift
@@ -26,5 +26,27 @@ class Predators {
             }
         }
     }
+    
+    func searc(for searchTerm: String) -> [ApexPredator] {
+        if searchTerm.isEmpty {
+            return apexPreadators
+        } else {
+            return apexPreadators.filter {
+                predator in
+                predator.name
+                    .localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool){
+        apexPreadators.sort { predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
+    }
 }
 
